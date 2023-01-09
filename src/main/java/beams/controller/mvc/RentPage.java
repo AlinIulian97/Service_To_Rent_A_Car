5a4a08f4b65b95d7613fae1fecc1945008a91dfd
AlinIulian97 <alin_iulia_2007@yahoo.com>
2023-01-09 14:49:37 +0200

    WebController

---
+++ src/main/java/beams/controller/mvc/RentPage.java
@@ -0,0 +1,14 @@
+package beams.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RentPage {
+
+    @GetMapping("/rentPage")
+    public String rentPage(){
+        return "rentPage";
+    }
+}
